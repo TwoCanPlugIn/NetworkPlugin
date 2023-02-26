856f73ca1c9295b9ca94abf2e1e76fc227e7e23c
twocanplugin <twocanplugin@hotmail.com>
2023-02-26 12:03:06 +1100

    Fix AUI

--- inc/network_pi_dialogbase.h
+++ inc/network_pi_dialogbase.h
@@ -1,5 +1,5 @@
 ///////////////////////////////////////////////////////////////////////////
-// C++ code generated with wxFormBuilder (version Oct 26 2018)
+// C++ code generated with wxFormBuilder (version 3.10.1-0-g8feb16b3)
 // http://www.wxformbuilder.org/
 //
 // PLEASE DO *NOT* EDIT THIS FILE!
@@ -15,7 +15,6 @@
 #include <wx/font.h>
 #include <wx/grid.h>
 #include <wx/gdicmn.h>
-#include <wx/sizer.h>
 #include <wx/panel.h>
 #include <wx/aui/aui.h>
 
@@ -32,7 +31,7 @@ class NetworkDialogBase : public wxPanel
 	protected:
 		wxGrid* gridNetwork;
 
-		// Virtual event handlers, overide them in your derived class
+		// Virtual event handlers, override them in your derived class
 		virtual void OnActivate( wxAuiManagerEvent& event ) { event.Skip(); }
 		virtual void OnClose( wxAuiManagerEvent& event ) { event.Skip(); }
 		virtual void OnInit( wxInitDialogEvent& event ) { event.Skip(); }
@@ -42,6 +41,8 @@ class NetworkDialogBase : public wxPanel
 	public:
 
 		NetworkDialogBase( wxWindow* parent, wxWindowID id = wxID_ANY, const wxPoint& pos = wxDefaultPosition, const wxSize& size = wxSize( 500,300 ), long style = wxTAB_TRAVERSAL, const wxString& name = wxEmptyString );
+		wxAuiManager m_mgr;
+
 		~NetworkDialogBase();
 
 };
--- src/network_pi_dialogbase.cpp
+++ src/network_pi_dialogbase.cpp
@@ -1,5 +1,5 @@
 ///////////////////////////////////////////////////////////////////////////
-// C++ code generated with wxFormBuilder (version Oct 26 2018)
+// C++ code generated with wxFormBuilder (version 3.10.1-0-g8feb16b3)
 // http://www.wxformbuilder.org/
 //
 // PLEASE DO *NOT* EDIT THIS FILE!
@@ -11,44 +11,38 @@
 
 NetworkDialogBase::NetworkDialogBase( wxWindow* parent, wxWindowID id, const wxPoint& pos, const wxSize& size, long style, const wxString& name ) : wxPanel( parent, id, pos, size, style, name )
 {
-	wxBoxSizer* sizerPanel;
-	sizerPanel = new wxBoxSizer( wxVERTICAL );
+	m_mgr.SetManagedWindow(this);
+	m_mgr.SetFlags(wxAUI_MGR_DEFAULT);
 
 	gridNetwork = new wxGrid( this, wxID_ANY, wxDefaultPosition, wxDefaultSize, 0 );
 
 	// Grid
 	gridNetwork->CreateGrid( 253, 3 );
-	gridNetwork->EnableEditing( false );
+	gridNetwork->EnableEditing( true );
 	gridNetwork->EnableGridLines( true );
 	gridNetwork->EnableDragGridSize( false );
 	gridNetwork->SetMargins( 0, 0 );
 
 	// Columns
-	gridNetwork->SetColSize( 0, 85 );
-	gridNetwork->SetColSize( 1, 120 );
-	gridNetwork->SetColSize( 2, 120 );
+	gridNetwork->SetColSize( 0, 30 );
+	gridNetwork->SetColSize( 1, 80 );
+	gridNetwork->SetColSize( 2, 80 );
 	gridNetwork->EnableDragColMove( false );
 	gridNetwork->EnableDragColSize( true );
-	gridNetwork->SetColLabelSize( 30 );
-	gridNetwork->SetColLabelValue( 0, wxT("Unique Id") );
-	gridNetwork->SetColLabelValue( 1, wxT("Manufacturer") );
-	gridNetwork->SetColLabelValue( 2, wxT("Model Id") );
-	gridNetwork->SetColLabelAlignment( wxALIGN_LEFT, wxALIGN_CENTER );
+	gridNetwork->SetColLabelAlignment( wxALIGN_CENTER, wxALIGN_CENTER );
 
 	// Rows
 	gridNetwork->EnableDragRowSize( true );
-	gridNetwork->SetRowLabelSize( 80 );
-	gridNetwork->SetRowLabelAlignment( wxALIGN_LEFT, wxALIGN_CENTER );
+	gridNetwork->SetRowLabelAlignment( wxALIGN_CENTER, wxALIGN_CENTER );
 
 	// Label Appearance
 
 	// Cell Defaults
 	gridNetwork->SetDefaultCellAlignment( wxALIGN_LEFT, wxALIGN_TOP );
-	sizerPanel->Add( gridNetwork, 0, wxALL, 5 );
+	m_mgr.AddPane( gridNetwork, wxAuiPaneInfo() .Left() .PinButton( true ).Dock().Resizable().FloatingSize( wxDefaultSize ) );
 
 
-	this->SetSizer( sizerPanel );
-	this->Layout();
+	m_mgr.Update();
 
 	// Connect Events
 	this->Connect( wxEVT_AUI_PANE_ACTIVATED, wxAuiManagerEventHandler( NetworkDialogBase::OnActivate ) );
@@ -64,5 +58,7 @@ NetworkDialogBase::~NetworkDialogBase()
 	this->Disconnect( wxEVT_AUI_PANE_CLOSE, wxAuiManagerEventHandler( NetworkDialogBase::OnClose ) );
 	this->Disconnect( wxEVT_INIT_DIALOG, wxInitDialogEventHandler( NetworkDialogBase::OnInit ) );
 	gridNetwork->Disconnect( wxEVT_GRID_CELL_RIGHT_CLICK, wxGridEventHandler( NetworkDialogBase::OnRightClick ), NULL, this );
+
+	m_mgr.UnInit();
 
 }
